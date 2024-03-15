35300305a51b0fe0cf9268a971dbf61f52f4920d
|-(ToT)-| <126763146+0x546F74@users.noreply.github.com>
2024-03-15 11:42:40 +0700

    add dex facet

--- contracts/facets/UniswapFacet.sol
+++ contracts/facets/UniswapFacet.sol
@@ -2,74 +2,13 @@
 pragma solidity ^0.8.19;
 
 import {LibDiamond} from "../libraries/LibDiamond.sol";
-import {IAllowanceTransfer} from '../permit2/src/interfaces/IAllowanceTransfer.sol';
-import {IPermit2} from '../permit2/src/interfaces/IPermit2.sol';
 import {IUniswapV2Router01} from '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router01.sol';
-import "../interfaces/IUniswapFacet.sol";
+import "../interfaces/IDexFacet.sol";
 import "../interfaces/IAccessFacet.sol";
 import "../interfaces/IManagerFacet.sol";
 import "../interfaces/IERC20.sol";
 
-contract UniswapFacet is IUniswapFacet {
-
-    function quickSwapUniversal(bytes memory _commands, bytes[] calldata _inputs) public payable {
-        IPermit2 permit2 = IPermit2(address(0x000000000022D473030F116dDEE9F6B43aC78BA3));
-        IERC20 WETH9 = IERC20(0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6);
-        uint[2] memory nativeData = calculateFeeAndRemain(msg.value);
-
-        // Consider to store fee native ETH in smart contract
-        address payable feeWallet = payable (IManagerFacet(address(this)).getFeeWallet());
-        (bool successTransferFee, ) = feeWallet.call{value: nativeData[1]}("");
-        require(successTransferFee, "Failed to send Ether");
-
-        address universalRouter = IManagerFacet(address(this)).getUniversalRouter();
-
-        uint8 commandTrack = 0;
-
-        // Execute Wrap to ETH
-        bytes1 command = _commands[commandTrack];
-        require(command == 0x0b, "Invalid wrap command");
-        bytes memory commandBytes = "\x0b";
-        bytes calldata commandInput = _inputs[commandTrack];
-        bytes[] memory commandInputArr = new bytes[](1);
-        commandInputArr[0] = commandInput;
-
-        (bool successWrapExecute, ) = address(universalRouter).call{value: nativeData[0]}(abi.encodeWithSignature("execute(bytes,bytes[])", commandBytes, commandInputArr));
-        require(successWrapExecute, "Failed to wrap ETH");
-        commandTrack += 1;
-
-        // Permit2 to universal router
-        command = _commands[commandTrack];
-        commandInput = _inputs[commandTrack];
-        commandInputArr[0] = commandInput;
-        if (command == 0x0a) {
-            IAllowanceTransfer.PermitSingle calldata permitSingle;
-            assembly {
-                permitSingle := commandInput.offset
-            }
-            bytes calldata signature = toBytes(commandInput, 6);
-            permit2.permit(msg.sender, permitSingle, signature);
-            commandTrack += 1;
-        }
-        command = _commands[commandTrack];   
-        commandInput = _inputs[commandTrack];
-        commandInputArr[0] = commandInput;
-
-        address WETHAddress = address(WETH9);
-
-        permit2.transferFrom(msg.sender, address(this), uint160(nativeData[0]), WETHAddress);
-
-        WETH9.approve(universalRouter, nativeData[0]);
-        WETH9.transferFrom(address(this), universalRouter, nativeData[0]);
-
-        require(command == 0x00, "Invalid swap command");
-        commandBytes = "\x00";
-
-        (bool successSwapExecute, ) = address(universalRouter).call(abi.encodeWithSignature("execute(bytes,bytes[])", commandBytes, commandInputArr));
-        require(successSwapExecute, "Failed to swap");
-
-        emit QuickSwapUniversal("Universal", msg.sender, msg.value, nativeData[1]);
-    }
+contract DexFacet is IDexFacet {
 
     function quickSwapV2In(address _v2Router, address _token0, address _token1, uint256 _amountOutMin, uint256 _deadline) external payable {
         uint[2] memory nativeData = calculateFeeAndRemain(msg.value);
--- contracts/interfaces/IDexFacet.sol
+++ contracts/interfaces/IDexFacet.sol
@@ -2,15 +2,11 @@
 pragma solidity ^0.8.19;
 
 
-interface IUniswapFacet {
-    
-    event QuickSwapUniversal(string _type, address _address, uint _amount, uint _fee);
+interface IDexFacet {
     
     event QuickSwapV2In(string _type, address _address, uint _amount, uint _fee);
 
     event QuickSwapV2Out(string _type, address _address, uint _amount, uint _fee);
-
-    function quickSwapUniversal(bytes memory _command, bytes[] memory _inputs) external payable;
 
     function quickSwapV2In(address _v2Router, address _token0, address _token1, uint256 _amountOutMin, uint256 _deadline) external payable;
 
